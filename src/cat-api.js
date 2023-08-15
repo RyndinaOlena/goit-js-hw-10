c52a816d9a2317c3e4bef47552864381c9bc2bd3
Ryndina Olena <ryndinaolena@gmail.com>
2023-08-15 10:11:38 +0300

    hw 9

---
+++ src/cat-api.js
@@ -0,0 +1,18 @@
+export { fetchBreeds }
+import axios from "axios";
+
+const API_KEY = axios.defaults.headers.common["x-api-key"] = "live_O4Nbn4iSQKDkMOFXZYeuCiyW4L0VJqoAZOulbPJHGGgpTLXIMLGudCOcOwO6HY2u";
+const API_URL = 'https://api.thecatapi.com/v1/breeds'
+
+
+function fetchBreeds() {
+    return axios.get(API_URL, API_KEY).then((responce) => {
+        console.log(responce)
+        if (responce.status !== 200) {
+            throw new Error(responce.statusText)
+        }
+        return responce.data
+    })
+}
+
+console.log(fetchBreeds())
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css'
+import { fetchBreeds } from './cat-api'
+const selectEl = document.querySelector('.breed-select')
+
+
+selectEl.addEventListener('change', getMurcup)
+
+
+fetchBreeds().then((data) => {
+    getMurcup(data)
+
+})
+
+function getMurcup(data) {
+    console.log(data)
+    // const takeCats = data.map(({ url, alt, name, descrition, temperament }) => {
+    //     `<li><img src="${url}" alt="${alt}"><h2>${name}</h2><p>${descrition}</p><h3>${temperament}</h3> </li>`
+    // })
+
+
+
+    const optionMurcup = data.map(({ id, name }) =>
+        `<option value="${id}">${name}</option>`
+    ).join('');
+    selectEl.insertAdjacentHTML('afterbegin', optionMurcup)
+
+    new SlimSelect({
+        select: document.querySelector('.breed-select'),
+    })
+
+
+    selectEl.insertAdjacentHTML('afterbegin', takeCats)
+
+}
+
+
+
+
+
