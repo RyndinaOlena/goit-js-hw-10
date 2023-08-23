71a2af374f01a5e7a8cc07add4715d5bc23c886f
Ryndina Olena <ryndinaolena@gmail.com>
2023-08-23 13:28:45 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -8,51 +8,70 @@ import Notiflix, { notiflix } from 'notiflix'
 
 const selectEl = document.querySelector('.breed-select')
 const catInfo = document.querySelector('.cat-info')
-const loaderEl = document.querySelector('.loaderS')
+const loaderEl = document.querySelector('.loader')
 const errorEl = document.querySelector('.error')
 errorEl.innerHTML = ''
-loaderEl.style.display = 'none'
+
 
 
 
 selectEl.addEventListener('change', onSelect)
+function onLoad() {
+    loaderEl.classList.remove('is-hidden')
+    fetchBreeds().then((data) => {
+        const optionMurcup = data.map(({ id, name }) =>
+            `<option value="${id}">${name}</option>`
+        ).join('');
+        selectEl.innerHTML = optionMurcup
 
-fetchBreeds().then((data) => {
-    const optionMurcup = data.map(({ id, name }) =>
-        `<option value="${id}">${name}</option>`
-    ).join('');
-    selectEl.innerHTML = optionMurcup
-
-    new SlimSelect({
-        select: document.querySelector('.breed-select'),
+        new SlimSelect({
+            select: document.querySelector('.breed-select'),
+        })
     })
-})
-    .catch(error => Notiflix.Report.failure('Oops! Something went wrong! Try reloading the page!'))
+        .catch((error) => onError()).finally(() =>
+            loaderEl.classList.add('is-hidden')
+        )
 
+}
+onLoad()
+
+
+function clearMarcup() {
+    catInfo.innerHTML = ""
+}
+
+function onError() {
+    Notiflix.Report.failure('Oops! Something went wrong! Try reloading the page!')
+}
 
 
 
 
 function onSelect(e) {
     const cat = e.target.value
-    // loaderEl.classList.add('loaderS')
-    loaderEl.style.display = 'block'
+
+    loaderEl.classList.remove('is-hidden')
+
+
     fetchCatByBreed(e.target.value).then((data) => {
         const events = e.target.value
+
 
 
         const createMurcup = data.filter(el => el.id === cat).map((el) => {
             return `<img src="${el.image.url}" alt="${el.name}" width="${el.image.width}" height="${el.image.heght}"/><h2>${el.name}</h2><p>${el.description}</p><p>${el.temperament}</p>
         `
         }).join('')
-        loaderEl.classList.remove('loaderS')
+
         catInfo.innerHTML = createMurcup
-        // if (catInfo !== '') {
-        //     loaderEl.classList.remove('loaderS')
-        // }
 
-    }).catch(error => Notiflix.Report.warning('Oops! Something went wrong! Try reloading the page!')).finally(() => {
 
+    }).catch(error => {
+        onError()
+        clearMarcup()
+    }).finally(() => {
+
+        loaderEl.classList.add('is-hidden')
     })
 }
 
